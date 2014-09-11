7a85fa2c2250db96306525448fe86766c3f14738
xxiem09 <xxiem09@yahoo.com>
2014-09-11 14:04:27 -0400

    Add lab 4 - to lab 7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController <ApplicationController
   def index
   	@available_at = Time.now
-  	@books = ["Atlas Shrugged", "Ender's Game", "Hamlet", "The Hobbit"]
+  	@books = Book.all
+  end
+  def show
+  	@book = Book.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Bookstore::Application.routes.draw do
  get "/books"=> "books#index", as: 'books'
+ get "/books/:id" => "books#show", as: 'book'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
