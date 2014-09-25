3626726fa1f6bee6a1e5d5cb7a474c43970f0302
xxiem09 <xxiem09@yahoo.com>
2014-09-25 18:41:27 -0400

    Lab 4-1 work

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ActiveRecord::Base
+	validates :title, :author, :pages, :price, presence: true
+validates :pages,
+numericality: { only_integer: true, greater_than_or_equal_to: 0 },
+unless: "pages.blank?"
+validates :price,
+numericality: { greater_than_or_equal_to: 0 },
+if: "price.present?"
 end
